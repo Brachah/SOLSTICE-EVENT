f942bc417767946366cd3c33bd7751b0014b7b08
Mary_Sam <marysamuel004@gmail.com>
2026-08-23 20:14:27 +0100

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,8 @@
+<script>
+  // Shared utility functions
+  function formatDate(date) {
+    return new Date(date).toLocaleDateString('en-US', {
+      year: 'numeric', month: 'short', day: 'numeric'
+    });
+  }
+</script>
